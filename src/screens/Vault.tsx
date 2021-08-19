ec37b1abe1cf21a59bef3f9849f7faed6ae501d1
GiraeffleAeffle <maximilian.stahl@gmx.de>
2021-08-19 21:50:32 +0200

    add Web3modal

--- src/screens/Vault.tsx
+++ src/screens/Vault.tsx
@@ -24,11 +24,29 @@ import {
 } from "@chakra-ui/react"
 import { useSetState } from "ahooks"
 import React, { useRef, useState, useEffect } from "react"
-
+import useWeb3Modal from "../hooks/useWeb3Modal";
 import { ProgressStatCard } from "../components/ProgressStatCard"
 import { aUSDCabi } from "../ABIs/aUSDCabi";
 import { usdcAbi } from "../ABIs/USDCabi";
 import { climateWarriorsAbi } from "../ABIs/climateWarriorsAbi";
+
+
+
+function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
+   return (
+     <Button 
+       onClick={() => {
+         if (!provider) {
+           loadWeb3Modal();
+         } else {
+           logoutOfWeb3Modal();
+         }
+       }}
+     >
+       {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+     </Button>
+   );
+ }
 
 interface VaultState {
    global: any
@@ -67,8 +85,11 @@ export const Vault: React.FC = () => {
       },
    })
 
+   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+
    useEffect(() => {
       // Update the document title using the browser API
+      
       contractBalance().then((res) => {
          console.log("useeffect: ", res);
 
@@ -99,6 +120,9 @@ export const Vault: React.FC = () => {
 
    return (
       <Container maxW="container.lg">
+         <Heading>
+            <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
+         </Heading>
          <SimpleGrid columns={2} spacing={8}>
             <GlobalFundraiseProgress {...state} />
             <DepositActions {...state.wallet} setState={setState} />
